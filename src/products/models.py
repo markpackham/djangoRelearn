fa5ab46e3429c9ed5b3ca24014a7b9c41092755e
mark <markpackham1@gmail.com>
2020-09-30 17:15:35 +0100

    create pages, https://youtu.be/F5mRW0jo-U4?t=3608

--- src/products/models.py
+++ src/products/models.py
@@ -6,4 +6,5 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.FloatField() 
     summary = models.TextField(blank=False, null=False)
+    features = models.BooleanField(default=True)
 
--- src/tryjango/settings.py
+++ src/tryjango/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # Third party
 
     # Own
+    'pages',
     'products',
 ]
 
