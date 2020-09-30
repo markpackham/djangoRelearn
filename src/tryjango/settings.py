7131d2f68b40e6d8abdd7c8b74539c5d1b72f6fb
mark <markpackham1@gmail.com>
2020-09-30 13:38:39 +0100

    created a product, https://youtu.be/F5mRW0jo-U4?t=2569

--- src/tryjango/settings.py
+++ src/tryjango/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party
+
+    # Own
+    'products',
 ]
 
 MIDDLEWARE = [
