8324aa708e152f1b316c671a1fc1c3caaeaae7e9
mark <markpackham1@gmail.com>
2020-10-01 12:12:33 +0100

    basic routing, https://youtu.be/F5mRW0jo-U4?t=4308

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(*args, **kwargs):
+    return  HttpResponse("<h1>Hello World</h1>")
+
+def contact_view(*args, **kwargs):
+    return  HttpResponse("<h1>Contact</h1>")
+
+def about_view(*args, **kwargs):
+    return  HttpResponse("<h1>About</h1>")
+
+def social_view(*args, **kwargs):
+    return  HttpResponse("<h1>Social Media</h1>")
