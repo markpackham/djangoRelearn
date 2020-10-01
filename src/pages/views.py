293ba222c82e3d43363bb2cc354caaf1e542e209
mark <markpackham1@gmail.com>
2020-10-01 16:52:49 +0100

    add templates, https://youtu.be/F5mRW0jo-U4?t=5259

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,14 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return  HttpResponse("<h1>Hello World</h1>")
+def home_view(request, *args, **kwargs):
+    #return  HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs):
-    return  HttpResponse("<h1>Contact</h1>")
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
 
-def about_view(*args, **kwargs):
-    return  HttpResponse("<h1>About</h1>")
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
 
 def social_view(*args, **kwargs):
     return  HttpResponse("<h1>Social Media</h1>")
