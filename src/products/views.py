8c560f662576ca2af222d4229394b93d425a2e0c
mark <markpackham1@gmail.com>
2020-10-02 13:03:29 +0100

    product specific template, https://youtu.be/F5mRW0jo-U4?t=7630

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
 
-# Create your views here.
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_detail.html", context)
--- src/tryjango/urls.py
+++ src/tryjango/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view, about_view, social_view
+from products.views import product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('admin/', admin.site.urls),
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
     path('social/', social_view, name='social'),
-    path('admin/', admin.site.urls),
+    path('products/', product_detail_view, name='product')
 ]
