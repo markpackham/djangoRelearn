81f2e366d52200ed78690c6c776cca0248e9ed81
mark <markpackham1@gmail.com>
2020-10-05 12:32:10 +0100

    add title validation, https://youtu.be/F5mRW0jo-U4?t=9967

--- src/products/forms.py
+++ src/products/forms.py
@@ -2,10 +2,45 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+
+    title = forms.CharField(label='',
+                            widget=forms.TextInput(attrs={'placeholder': 'Your title'
+                            }))
+    description = forms.CharField(required=False,
+                                  widget=forms.Textarea(attrs={
+        'placeholder': 'Your description',
+        'class': 'new-class-name two',
+        'id': 'my-id-for-textarea',
+        'rows': 20,
+        'cols': 120,
+        }))
+
+    price = forms.FloatField(initial=199.99)
+
     class Meta:
         model = Product
-        fields = [
-            'title',
-            'description',
-            'price'
-        ]
+        fields = ['title', 'description', 'price']
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data['title']
+        if "CFE" in title:
+            return title
+        else:
+            raise forms.ValidationError("This is not a valid title")
+
+
+class RawProductForm(forms.Form):
+
+    title = forms.CharField(label='',
+                            widget=forms.TextInput(attrs={'placeholder': 'Your title'
+                            }))
+    description = forms.CharField(required=False,
+                                  widget=forms.Textarea(attrs={
+        'placeholder': 'Your description',
+        'class': 'new-class-name two',
+        'id': 'my-id-for-textarea',
+        'rows': 20,
+        'cols': 120,
+        }))
+
+    price = forms.FloatField(initial=199.99)
