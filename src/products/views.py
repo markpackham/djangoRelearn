e7e1a73ca9b6d711cea035c27e581cdd40b2b764
mark <markpackham1@gmail.com>
2020-10-05 15:45:35 +0100

    create product list, https://youtu.be/F5mRW0jo-U4?t=10803

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import ProductForm, RawProductForm
 from .models import Product
@@ -21,7 +22,28 @@ def product_create_view(request):
     return render(request, "products/product_create.html", context)
 
 def dynamic_lookup_view(request, id):
-    obj = Product.objects.get(id=1)
-    context = {}
+    #obj = Product.objects.get(id=1)
+        # try:
+    #     obj = Product.objects.get(id=id)
+    # except Product.DoesNotExist:
+    #     raise Http404
+    obj = get_object_or_404(Product, id=id)
+    context = {"object": obj}
     return render(request, "products/product_detail.html", context)
- 
+
+def product_list_view(request):
+    queryset = Product.objects.all() # list of objects
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "products/product_list.html", context)
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == "POST":
+        obj.delete()
+        return redirect('../../')
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_delete.html", context)
--- src/tryjango/urls.py
+++ src/tryjango/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view, about_view, social_view
-from products.views import product_detail_view, product_create_view, dynamic_lookup_view
+from products.views import product_detail_view, product_create_view, product_delete_view, dynamic_lookup_view, product_list_view
 
 urlpatterns = [
     path('', home_view, name='home'),
@@ -24,7 +24,8 @@ urlpatterns = [
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
     path('social/', social_view, name='social'),
-    # path('products/', product_detail_view, name='product'),
-    path('products/<int:id>/', dynamic_lookup_view, name='products'),
+    path('products/', product_list_view, name='products'),
+    path('products/<int:id>/', dynamic_lookup_view, name='product'),
     path('create/',product_create_view, name='create'),
+    path('products/<int:id>/delete/', product_delete_view, name='product-delete'),
 ]
