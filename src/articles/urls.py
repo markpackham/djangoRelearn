ce654fd9ede9bb1c76f4097b646925fb56fd7576
mark <markpackham1@gmail.com>
2020-10-06 18:18:21 +0100

    show article list view, https://youtu.be/F5mRW0jo-U4?t=11449

--- src/articles/urls.py
+++ src/articles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from articles.views import article_list_view, article_detail_view
+# from articles.views import article_list_view, article_detail_view
+from .views import ArticleListView
 
 app_name = 'articles'
 urlpatterns = [
-    path('', article_list_view, name='article-list'),
-    path('<int:id>/', article_detail_view, name='article-detail'),
+    # path('', article_list_view, name='article-list'),
+    # path('<int:id>/', article_detail_view, name='article-detail'),
+    path('', ArticleListView.as_view(), name='article-list'),
 ]
--- src/articles/views.py
+++ src/articles/views.py
@@ -3,19 +3,21 @@ from django.views.generic import CreateView, DetailView, ListView, UpdateView, D
 from .forms import ArticleForm
 from .models import Article
 
-def article_list_view(request):
-    queryset = Article.objects.all()
-    context = {
-        "object_list": queryset
-    }
-    return render(request, "articles/article_list.html", context)
+# def article_list_view(request):
+#     queryset = Article.objects.all()
+#     context = {
+#         "object_list": queryset
+#     }
+#     return render(request, "articles/article_list.html", context)
 
-def article_detail_view(request, id):
-    obj = get_object_or_404(Article, id=id)
-    context = {
-        "object": obj
-    }
-    return render(request, "articles/article_detail.html", context)
+# def article_detail_view(request, id):
+#     obj = get_object_or_404(Article, id=id)
+#     context = {
+#         "object": obj
+#     }
+#     return render(request, "articles/article_detail.html", context)
 
+# articles/<modelname>_list.html
 class ArticleListView(ListView):
+    template_name = 'articles/article_list.html'
     queryset = Article.objects.all()
