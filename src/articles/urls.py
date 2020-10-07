e3d3430cf7961a967ba4f01ad8e1ecd764edd56d
mark <markpackham1@gmail.com>
2020-10-07 13:35:43 +0100

    use DetailView, https://youtu.be/F5mRW0jo-U4?t=11742

--- src/articles/urls.py
+++ src/articles/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 # from articles.views import article_list_view, article_detail_view
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 app_name = 'articles'
 urlpatterns = [
     # path('', article_list_view, name='article-list'),
     # path('<int:id>/', article_detail_view, name='article-detail'),
     path('', ArticleListView.as_view(), name='article-list'),
+    # the default to look for is pk(primary key) or slug & not id
+    # path('<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
+
+
 ]
--- src/articles/views.py
+++ src/articles/views.py
@@ -21,3 +21,11 @@ from .models import Article
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     queryset = Article.objects.all()
+
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    #queryset = Article.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
